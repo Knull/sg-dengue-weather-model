78d92d79df7f6a326dd195c95c21f8dbe65a97fe
Knull <70634125+Knull@users.noreply.github.com>
2025-08-12 15:03:39 +0800

    Create __init__.py

---
+++ src/dengueweather/__init__.py
@@ -0,0 +1,16 @@
+"""package for the Dengue-Weather analysis.
+
+This package contains modules for data ingestion, feature engineering,
+modelling and visualisation.  See subpackages for specific functionality.
+"""
+
+__all__ = [
+    "paths",
+    "config",
+    "utils_geo",
+    "utils_time",
+    "ingest",
+    "build",
+    "model",
+    "viz",
+]
